dbc51828cf62f117ec04714808e4d4674d54d15f
noaprost <kangjiwon1850@gmail.com>
2024-03-10 22:28:28 +0900

    feat: Post Card 구현, Home - Featured Posts 구현

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  id: string;
+  image: string;
+  day: string;
+  title: string;
+  description: string;
+  category: string;
+};
+
+export default function PostCard({
+  id,
+  image,
+  day,
+  title,
+  description,
+  category,
+}: Props) {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/posts/${id}`);
+  };
+  return (
+    <div
+      className="flex flex-col w-80 h-80 items-center justify-between bg-neutral-900 p-4 rounded-2xl shadow-custom cursor-pointer"
+      onClick={handleClick}
+    >
+      <Image
+        src={`/images/${image}`}
+        alt="title"
+        width={280}
+        height={100}
+        className="w-full h-44 rounded-lg"
+      />
+      <div>
+        <p className="text-xs mt-1 text-gray-500">{day}</p>
+      </div>
+      <h2 className="font-semibold text-sm">{title}</h2>
+      <p className="text-xs">{description}</p>
+      <p className="text-xs bg-violet-900 px-1 rounded-md">{category}</p>
+    </div>
+  );
+}
