1c9dc1c67e6f32b65f6946e6d4ccbce9eec5f1d6
noaprost <kangjiwon1850@gmail.com>
2024-03-10 23:32:55 +0900

    feat: Posts 페이지 구현

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,19 @@
+import PostsByCategory from "@/components/PostsByCategory";
+
 export default function PostsPage() {
-  // 전체 포스트 카드 목록 구현
-  return <h1>Posts!</h1>;
+  return (
+    <div className="flex h-max">
+      <PostsByCategory category="backend" />
+      <div className="w-1/4 h-72 flex flex-col justify-evenly items-center mt-24 mx-auto">
+        <p className="font-bold underline underline-offset-8">Category</p>
+        <p className="cursor-pointer font-light hover:scale-110">All Posts</p>
+        <p className="cursor-pointer font-light hover:scale-110">algorithm</p>
+        <p className="cursor-pointer font-light hover:scale-110 text-orange-400">
+          backend
+        </p>
+        <p className="cursor-pointer font-light hover:scale-110">frontend</p>
+        <p className="cursor-pointer font-light hover:scale-110">study</p>
+      </div>
+    </div>
+  );
 }
