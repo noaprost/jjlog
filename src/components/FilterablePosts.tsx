190d408b8f4eca2722572083d6fc28ef4a98e61e
noaprost <kangjiwon1850@gmail.com>
2024-03-11 23:48:08 +0900

    feat: Post Page category 별로 filter되는 부분 구현

---
+++ src/components/FilterablePosts.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState } from "react";
+import PostCard from "./PostCard";
+import Category from "./Category";
+
+type Props = {
+  posts: any[];
+  categories: string[];
+};
+const ALL_POSTS = "All Posts";
+
+export default function FilterablePosts({ posts, categories }: Props) {
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filterd =
+    selected === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category === selected);
+  const len = Math.ceil(filterd.length / 3);
+  return (
+    <section className="flex">
+      <div
+        className={`grid grid-cols-3 grid-rows-${
+          len > 2 ? len : 2
+        } gap-7 py-8 pl-8`}
+      >
+        {filterd.map((post) => (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            image={post.image}
+            day={post.day}
+            title={post.title}
+            description={post.description}
+            category={post.category}
+          />
+        ))}
+      </div>
+      <div className="mx-auto">
+        <Category
+          categories={[ALL_POSTS, ...categories]}
+          selected={selected}
+          onClick={setSelected}
+        />
+      </div>
+    </section>
+  );
+}
