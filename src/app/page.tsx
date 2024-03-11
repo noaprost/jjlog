b9e92bf0d8255802785b0e516546022f1992b69e
noaprost <kangjiwon1850@gmail.com>
2024-03-11 21:59:42 +0900

    feat: Home Page Post Slide 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import PostCard from "@/components/PostCard";
 import Profile from "@/components/Profile";
+import Slide from "@/components/Slide";
 import { getPostCard } from "@/service/posts";
 
 export default async function Home() {
@@ -8,7 +9,7 @@ export default async function Home() {
   return (
     <div>
       <Profile />
-      <h1 className="text-xl font-bold pl-12 pt-12 pb-4">Featured Posts</h1>
+      <h1 className="text-2xl font-bold pl-12 pt-12 pb-8">Featured Posts</h1>
       <div className="grid grid-cols-4 grid-rows-2 gap-y-8 px-12">
         {postCards
           .filter((card) => card.featured)
@@ -23,6 +24,22 @@ export default async function Home() {
               category={card.category}
             />
           ))}
+      </div>
+      <h1 className="text-2xl font-bold pl-12 pt-24">You may like</h1>
+      <div className="p-12">
+        <Slide>
+          {postCards.map((card) => (
+            <PostCard
+              key={card.id}
+              id={card.id}
+              image={card.image}
+              day={card.day}
+              title={card.title}
+              description={card.description}
+              category={card.category}
+            />
+          ))}
+        </Slide>
       </div>
     </div>
   );
