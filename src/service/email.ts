74814d9ca97b3564e19426eb97f7ac1bcd7debdd
noaprost <kangjiwon1850@gmail.com>
2024-03-22 00:08:24 +0900

    feat: email 전송 UI 및 기능 구현

---
+++ src/service/email.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+export type EmailData = {
+  email: string;
+  subject: string;
+  message: string;
+};
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.AUTH_USER,
+    pass: process.env.AUTH_PASS,
+  },
+});
+
+// 외부에서 이용할 수 있는 함수
+export async function sendEmail({ email, subject, message }: EmailData) {
+  const mailData = {
+    from: email,
+    to: process.env.AUTH_USER,
+    subject: `[BLOG] ${subject}`,
+    html: `
+    <h1>${subject}</h1>
+    <div>${message}</div>
+    <br/>
+    <p>보낸 사람 : ${email}</p>
+    `,
+  };
+
+  return transporter.sendMail(mailData);
+}
