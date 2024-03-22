a4fbead7d8a2f5081ef81a5216a6e13488f20678
noaprost <89311925+noaprost@users.noreply.github.com>
2024-03-22 18:50:15 +0900

    Release (#20)
    * feat: header 구성 변경
    
    * chore: 디자인 수정
    
    * chore: 글씨체 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,17 +4,17 @@ export default function AboutPage() {
   return (
     <div>
       <Profile />
-      <div className="bg-neutral-200 dark:bg-neutral-900 h-64 p-6 flex flex-col justify-between items-center mt-6">
-        <div className="flex flex-col items-center">
+      <div className="bg-neutral-200 dark:bg-neutral-900 p-3 flex flex-col items-center">
+        <div className="flex flex-col items-center p-1">
           <h2 className="font-semibold text-md mb-1">Who are we?</h2>
           <p className="text-sm">꿈을 향해 코딩하는 개발자</p>
           <p className="text-sm">취미는 알고리즘 문제 풀기</p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center p-1">
           <h2 className="font-semibold text-md mb-1">Career</h2>
           <p className="text-sm">인천대학교 졸업</p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center p-1">
           <h2 className="font-semibold text-md mb-1">Skills</h2>
           <p className="text-sm">Jae Cheol - Java, Spring, JPA</p>
           <p className="text-sm">Ji Won - React, Typescript, Next.js</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <div>
       <Profile />
-      <h1 className="text-2xl font-bold pl-12 pt-12 pb-8">Featured Posts</h1>
+      <h1 className="text-2xl font-bold pl-12 pb-8">Featured Posts</h1>
       <div
         className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-8 px-12`}
       >
