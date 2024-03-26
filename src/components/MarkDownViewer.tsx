53c7e5022c8690abedd257071946d24958b15630
noaprost <kangjiwon1850@gmail.com>
2024-03-27 00:07:44 +0900

    feat: 데이터 추가

--- src/components/MarkDownViewer.tsx
+++ src/components/MarkDownViewer.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import Image from "next/image";
+//import Image from "next/image";
 import {
-  coldarkDark,
   synthwave84,
+  okaidia,
 } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkDownViewer({ content }: { content: string }) {
@@ -21,7 +21,7 @@ export default function MarkDownViewer({ content }: { content: string }) {
           return match ? (
             <SyntaxHighlighter
               {...rest}
-              style={synthwave84}
+              style={okaidia}
               language={match[1]}
               PreTag="div"
               ref={null}
