8f2ca62dafb3136788c2604597997bdf175cdbaf
noaprost <kangjiwon1850@gmail.com>
2024-03-26 21:02:58 +0900

    feat: 코드 하이라이트 기능 추가

--- src/components/MarkDownViewer.tsx
+++ src/components/MarkDownViewer.tsx
@@ -1,10 +1,50 @@
+"use client";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import Image from "next/image";
+import {
+  coldarkDark,
+  synthwave84,
+} from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkDownViewer({ content }: { content: string }) {
   return (
-    <ReactMarkdown className="prose-base" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose-base"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code(props) {
+          const { children, className, node, ref, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              style={synthwave84}
+              language={match[1]}
+              PreTag="div"
+              ref={null}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="w-full max-h-60 object-cover"
+            src={image.src || ""}
+            alt={image.alt || ""}
+            width={500}
+            height={350}
+          />
+        ),
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
