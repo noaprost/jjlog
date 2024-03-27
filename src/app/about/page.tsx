b087a33ac0f2d997ae3b3f45622c5396470f8028
noaprost <89311925+noaprost@users.noreply.github.com>
2024-03-28 06:58:36 +0900

    Release (#39)
    * feat: featured 포스트 UI 슬라이드로 바꾸기
    
    * feat: about 페이지 UI 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,11 @@ import Profile from "@/components/Profile";
 
 export default function AboutPage() {
   return (
-    <div>
-      <Profile />
-      <div className="bg-neutral-200 dark:bg-neutral-900 p-3 flex flex-col items-center">
+    <div className="flex pt-16 pb-28 px-24">
+      <div className="w-full flex items-center justify-center">
+        <Profile />
+      </div>
+      <div className="bg-orange-100 dark:bg-neutral-900 p-3 flex flex-col items-center w-full justify-center rounded-lg">
         <div className="flex flex-col items-center p-1">
           <h2 className="font-semibold text-md mb-1">Who are we?</h2>
           <p className="text-sm">꿈을 향해 코딩하는 개발자</p>
