4a62ddad14b908cd62f499dda1f3fbd8f32a2b16
noaprost <kangjiwon1850@gmail.com>
2024-03-28 18:49:27 +0900

    feat: 핀찍기 카운트 초과시 팝업

--- src/components/FeaturedButton.tsx
+++ src/components/FeaturedButton.tsx
@@ -5,13 +5,27 @@ import { FaStar } from "react-icons/fa";
 
 export default function FeaturedButton({ post }: { post: PostData }) {
   const [featuredPost, setFeaturedPost] = useState<PostData>(post);
+  const pinCount: number = 4;
   const handleClick = () => {
-    setFeaturedPost({
-      ...featuredPost,
-      featured: !featuredPost.featured,
-    });
-    // 데이터 보내서 수정
-    console.log(post);
+    if (featuredPost.featured) {
+      setFeaturedPost({
+        ...featuredPost,
+        featured: !featuredPost.featured,
+      });
+      // 데이터 보내서 수정
+      console.log(post);
+    } else {
+      if (pinCount == 4) {
+        alert("pin은 4개까지만 등록할 수 있습니다.");
+      } else {
+        setFeaturedPost({
+          ...featuredPost,
+          featured: !featuredPost.featured,
+        });
+        // 데이터 보내서 수정
+        console.log(post);
+      }
+    }
   };
   return (
     <FaStar
