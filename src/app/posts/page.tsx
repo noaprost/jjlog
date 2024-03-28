c31cd79c3de0ea4b44230e432339ecd71bf22f9d
noaprost <89311925+noaprost@users.noreply.github.com>
2024-03-29 00:53:48 +0900

    Release (#43)
    * feat: 카테고리 고정으로 변경하기

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,13 +4,7 @@ import { getPostCard } from "@/service/posts";
 export default async function PostsByCategory() {
   const postCards = await getPostCard();
   const sortedPostCard = postCards.sort((a, b) => Number(b.id) - Number(a.id));
-  const filterd = sortedPostCard.map((post) => post.category);
-  let categoryies: string[] = [];
-  filterd.map((item) => {
-    if (!categoryies.includes(item)) {
-      categoryies.push(item);
-    }
-  });
+  const categoryies: string[] = ["backend", "frontend", "algorithm", "study"];
 
   return (
     <div>
