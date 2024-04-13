aca52c91943882a166041301004acac61744527b
noaprost <89311925+noaprost@users.noreply.github.com>
2024-04-13 21:05:59 +0900

    Release (#63)
    * feat: 서버 주소 변경

--- src/components/ModifyForm.tsx
+++ src/components/ModifyForm.tsx
@@ -130,7 +130,7 @@ export default function ModifyForm({ post }: { post: PostData }) {
       <textarea
         required
         name="content"
-        className="p-2  outline-neutral-200 outline-offset-1 rounded-md mb-4 bg-neutral-100 dark:bg-neutral-900"
+        className="p-2 outline-neutral-200 outline-offset-1 rounded-md mb-4 bg-neutral-100 dark:bg-neutral-900"
         placeholder="내용 / 마크다운 문법을 사용해 입력해주세요"
         rows={20}
         value={modifyPost.content}
--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://ec2-44-218-178-139.compute-1.amazonaws.com:8080/api",
+  baseURL: "https://jaefe.store/api",
 });
 
 export default API;
