94b23435ee9d130c25cdf205dd86f6636d1a8a15
noaprost <kangjiwon1850@gmail.com>
2024-04-13 21:04:57 +0900

    feat: 서버 주소 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,12 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </head>
+      <head></head>
       <body className={inter.className}>
         <AuthProvider>
           <Header />
