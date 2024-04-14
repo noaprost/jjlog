3e2af9decf5a2c18fb0d979d1647e418d3737c60
noaprost <kangjiwon1850@gmail.com>
2024-04-15 00:50:57 +0900

    feat: 로그 삭제

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -31,11 +31,9 @@ export const AuthProvider: React.FC<{
       },
     }).then((data) => {
       if (data.data.mid !== "anonymous") {
-        console.log("여기");
         setUser(true);
         setUserName(data.data.mid);
       } else {
-        console.log("저기");
         setUser(false);
         setUserName("");
       }
