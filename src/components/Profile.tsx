1f76ce7d201aeac6bd22bbc58011188ffb10c926
noaprost <kangjiwon1850@gmail.com>
2024-04-15 16:29:37 +0900

    feat: 반응형으로 변경

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,9 +5,13 @@ export default function Profile() {
   return (
     <div className="w-72 h-72 flex flex-col justify-between items-center mx-auto my-6">
       <ProfileImage width={150} height={150} />
-      <h2 className="text-lg font-semibold">Hello, We&apos;re JJ</h2>
-      <p className="text-sm">Back-end & Front-end engineer</p>
-      <p className="text-sm">꾸준히 성장하는 개발자</p>
+      <h2 className="lg:text-lg sm:text-md text-md font-semibold">
+        Hello, We&apos;re JJ
+      </h2>
+      <p className="lg:text-sm sm:text-xs text-xs">
+        Back-end & Front-end engineer
+      </p>
+      <p className="lg:text-sm sm:text-xs text-xs">꾸준히 성장하는 개발자</p>
       <ContactButton />
     </div>
   );
