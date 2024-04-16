bebdd6e271678491ea6907ad5113c874c261ad34
noaprost <kangjiwon1850@gmail.com>
2024-04-16 18:35:49 +0900

    chore: 자잘한것 수정

--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import New from "@/components/New";
-import { useAuthContext } from "@/context/AuthContext";
 
 export default function NewPostPage() {
-  const { getUserName } = useAuthContext();
-  const name = getUserName();
-  return <New name={name} />;
+  return <New />;
 }
