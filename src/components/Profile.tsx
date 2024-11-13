1a76f0c1f65257cbae21ad6553c3513c1979ad39
noaprost <kangjiwon1850@gmail.com>
2024-11-13 22:31:04 +0900

    feat: 프로필 타이틀 수정

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,12 +5,11 @@ export default function Profile() {
   return (
     <div className="w-72 h-72 flex flex-col justify-between items-center mx-auto my-6">
       <ProfileImage width={150} height={150} />
-      <a href="https://git.io/typing-svg" className="w-72 mt-4 ml-16">
-        <img
-          src="https://readme-typing-svg.demolab.com?font=Fira+Code&size=36&duration=2000&pause=1000&color=000000&width=435&lines=Hello%2C+We're+JJ"
-          alt="Typing SVG"
-        />
-      </a>
+      <img
+        src="https://readme-typing-svg.demolab.com?font=Fira+Code&size=36&duration=2000&pause=1000&color=000000&width=435&lines=Hello%2C+We're+JJ"
+        alt="Typing SVG"
+        className="w-72 mt-4 ml-16"
+      />
       <p className="lg:text-sm sm:text-xs text-xs">
         Back-end & Front-end engineer
       </p>
